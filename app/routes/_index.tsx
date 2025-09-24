5c73e79ec80d2366188c08dd0dd92336dcc50d6e
karthik-excrin <karthik@excrin.com>
2025-09-24 23:43:21 +0530

    Fix missing closing div tag

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -47,5 +47,6 @@ export default function Index() {
         </div>
       </div>
     </div>
+    </div>
   );
 }
--- public/build/_shared/remix_hmr-CMXAF3XX.js
+++
@@ -1,9 +0,0 @@
-import {
-  createHotContext
-} from "/build/_shared/chunk-U5E2PCIK.js";
-import "/build/_shared/chunk-UWV35TSL.js";
-import "/build/_shared/chunk-PNG5AS42.js";
-export {
-  createHotContext
-};
-//# sourceMappingURL=/build/_shared/remix_hmr-CMXAF3XX.js.map
